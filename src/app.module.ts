aab0b9ec4d7f28fdd405b5b7309826dfebab5857
rnmtp111 <55239560+rnmtp111@users.noreply.github.com>
2019-12-09 16:20:34 +0800

    Add files via upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { NewsModule } from './news/news.module';
+import { StudentModule } from './student/student.module';
+
+
 
 @Module({
-  imports: [],
+  imports: [UserModule, NewsModule, StudentModule],
   controllers: [AppController],
   providers: [AppService],
 })
